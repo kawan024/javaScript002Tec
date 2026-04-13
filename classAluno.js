8a9042e179bd308ec336428454b0cb73c5ec451e
kawan024 <kawan.rodrigues.santos@escola.pr.gov.br>
2026-04-13 17:09:40 +0000

    corriji o erro

--- classAluno.js
+++ classAluno.js
@@ -1,18 +1,31 @@
-class Aluno{
-    constructor(nome, matricula, notas){
-        this.nome = nome;
-        this.matricula = matricula;
-        this.notas = notas;
-    }
+class Aluno {
+  constructor(nome, matricula, notas) {
+    this.nome = nome;
+    this.matricula = matricula;
+    this.notas = notas;
+  }
 
-    calculaMedia(){
-        let soma = 0;
-        for(let i = 0; i < this.notas.length; i++){
-            soma += this.notas[i];
-        }
-        return soma / this.notas.length;
+  calculaMedia() {
+    let soma = 0;
+    for (let i = 0; i < this.notas.length; i++) {
+      soma += this.notas[i];
     }
+    return soma / this.notas.length;
+  }
+
+  obterStatus() {
+    const media = this.calculaMedia();
+
+    if (media >= 7) {
+      return "aprovado";
+    } else if (media >= 5) {
+      return "recuperação";
+    } else {
+      return "reprovado";
+    }
+  }
 }
-   
+
 let aluno1 = new Aluno("ana", 5050, [8, 0, 7, 3, 9, 0]);
 console.log(aluno1.calculaMedia());
+console.log(aluno1.obterStatus());
