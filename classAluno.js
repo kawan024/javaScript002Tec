634dd08e00e318ab8f7e1a9c340e3311b9a2fa1f
kawan024 <kawan.rodrigues.santos@escola.pr.gov.br>
2026-04-15 13:01:51 +0000

    acabei

--- classAluno.js
+++ classAluno.js
@@ -24,8 +24,35 @@ class Aluno {
       return "reprovado";
     }
   }
+
+  exibirRelatorio() {
+    console.log(`=================================`);
+    console.log(`Relatório do aluno: ${this.nome}`);
+    console.log(`Matrícula: ${this.matricula}`);
+    console.log(`Média final: ${this.calculaMedia()}`);
+    console.log(`Status: ${this.obterStatus()}`);
+    console.log(`=================================`);
+  }
 }
 
-let aluno1 = new Aluno(`ana`, 5050, [8, 0, 7, 3, 9, 0]);
-console.log(aluno1.calculaMedia());
-console.log(aluno1.obterStatus());
+let turma = [
+  new Aluno(`Luiz`, `CGM5050`, [5.2, 9.5, 75]),
+  new Aluno(`Kawan`, `CGM6050`, [7.2, 7.5, 62]),
+  new Aluno(`Ana`, `CGM7050`, [4.2, 6.7, 82]),
+  new Aluno(`Cauan`, `CGM3050`, [3.2, 9.7, 92]),
+  new Aluno(`Fernando`, `CGM4050`, [6.5, 8.5, 72]),
+]
+
+
+console.log(turma[1]);
+
+for (let i = 0; i <turma.length; i++ ){
+  turma[i].exibirRelatorio();
+}
+
+//let aluno1 = new Aluno("ana", 5050, [8, 0, 7, 3, 9, 0]);
+//console.log(aluno1.calculaMedia());
+//console.log(aluno1.obterStatus());
+
+//aluno1.matricula = 5000000
+//aluno1.exibirRelatorio()
